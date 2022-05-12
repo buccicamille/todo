912d1c4e318a4946396093260e481c5f5370faa6
Camille <bucci.camille7@gmail.com>
2022-05-12 20:34:02 -0300

    small modifications to gitignore file and setup.sh file

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 # git config --global user.name Luiz Gomes
 
 # setup python virtual enviroments.
-python -m venv venv
-source venv/bin/activate
+python -m venv .venv
+source .venv/bin/activate
 python -m pip install --upgrade pip
 pip install -r requirements.txt
